700c5fefaa777d1685370635646c951480456d37
Maya Chisem <chisem.maya@gmail.com>
2020-02-09 19:02:05 -0800

    update

--- style.js
+++ style.js
@@ -9,29 +9,73 @@ function weatherDaily(cityValue) {
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityValue+"&appid=aa5b71088d417c59bb0ed00cd6cfe06e"
 
-    // Here we run our AJAX call to the OpenWeatherMap API
-    $.ajax({
-        url: queryURL,
+    var citiesArray = JSON.parse(localStorage.getItem('city')) || [];
+
+    $(document).ready(function() {
+      
+      var city = citiesArray[citiesArray.length - 1];
+      fiveDay(city);
+      citySearch(city);
+    });
+
+     // clear out previous city data
+     $(".city-name").empty();
+     $(".temperature").empty();
+     $(".humidity").empty();
+     $(".wind").empty();
+     $(".uv-index").empty();
+     var citySearch = queryURL;
+     console.log(citySearch);
+
+      // ajax for searching for new city to display
+      $.ajax({
+        url: citySearch,
         method: "GET"
-      }).then(function(response){
-        console.log(response);
-      })
-}
+      }).then(function(response) {
+        // * Display the following under current weather conditions:
+        //  line one
+        //   * City
+        var cityInfo = response.name;
+        console.log(cityInfo);
+        //   * Icon image (visual representation of weather conditions)
+        // Where are we pulling the icons from and how
+        var iconDummy = "https://openweathermap.org/img/wn/";
+        var iconPng = "@2x.png";
+        var iconWeather = response.weather[0].icon;
+        var iconUrl = iconDummy + iconWeather + iconPng;
+        console.log(iconUrl);
+        var iconImg = $("<img>");
+        iconImg.attr("src", iconUrl);
+        $(".city").append(cityInfo + " ");
+        $(".city").append(iconImg);
+        // line two
+        //   * Temperature
+        // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
+        console.log(response.main.temp);
+        var K = response.main.temp;
+        console.log(K);
+        var F = ((K - 273.15) * 1.8 + 32).toFixed(0);
+        console.log(F);
+        $(".temperature").append("Temperature: " + F + " °F");
+        //   * UV index
+        // PULL LON/LAT INFO REPONSE.COORD.LON AND RESPONSE.COORD.LAT
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+        // SEND OVER TO uvIndex()
+        uvIndex(lon, lat);
+      });
+    }
 
 
+//on click function, saving the city value in local storage
 $(".btn").on('click', function(){
-    console.log("hello");
+    console.log("good job!");
     var cityValue = $(".city").val().trim();
     localStorage.setItem('city', cityValue);
 
-    var cityButton = $("<div>");
-    $(".city").append(cityButton);
+    // var cityArray = JSON.parse(localStorage.getItem('city')) || [];
 
     // $(".city").val(localStorage.getItem('city')); 
 
     weatherDaily(cityValue);
 })
-
-
-
- 
