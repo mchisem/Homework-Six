2fabc5efabbe3555fe0fb8646ac1f494aad4738f
Maya Chisem <chisem.maya@gmail.com>
2020-02-09 17:48:05 -0800

    update

--- style.js
+++ style.js
@@ -1,17 +1,42 @@
 //I want to date to display//
 $('.time').text(moment().format("dddd, MMM Do, YYYY")); 
 
-//api url//
-var city = "";
-var state = "";
-var querURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + state + "&appid=fa0bccc8";
+// API key
+// var APIKey = "94fb4992412a398a9fb2333272321439";
+// var city = $(".city").val().trim();
+// var state = "";
+
+// Here we are building the URL we need to query the database
+function weatherDaily(cityValue) {
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityValue+"&appid=aa5b71088d417c59bb0ed00cd6cfe06e"
+
+
+
+    // Here we run our AJAX call to the OpenWeatherMap API
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response){
+        console.log(response);
+
+      })
+}
+
 
 $(".btn").on('click', function(){
     console.log("hello");
     var cityValue = $(".city").val();
     localStorage.setItem('city', cityValue);
 
+    var cityButton = $("<div>");
+    $(".city").append(cityButton);
+
+    // $(".city").val(localStorage.getItem('city')); 
+
+    weatherDaily(cityValue);
 })
+
 
 
  
